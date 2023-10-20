096eb4d32aeae59cb5c07b3f9db64d7eb329e62d
Irsal Lazuard Izzatillah <91246036+IrsalLaz@users.noreply.github.com>
2023-10-21 01:09:04 +0700

    WeatherClient - split input and access weather url

--- apps/05_weather_client/WeatherClient.py
+++ apps/05_weather_client/WeatherClient.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
+import collections
+
+Location = collections.namedtuple('Location', 'city state')
+
 
 def main():
 
@@ -10,10 +14,54 @@ def main():
     # input location
     location = input("Enter your location(e.g Portland, OR, US): ")
 
-    call_weather(location)
-def call_weather(location):
-    # get weather url
-    base_URL = f"https://www.wunderground.com/weather/id/{location}"  #TODO: change id to nation_code later
+    # split information from input
+    loc = location_splitter(location)
+
+    print(loc.city)
+
+    access_weather_web(loc)
+
+    # access the weather web
+
+    # if "," in location:
+    #     location_list = location.split(", ")
+    #     city, nation = location_list
+    #
+    # print(city)
+    # print(nation)
+
+    # call_weather(location)
+
+
+def location_splitter(location):
+    # check if the arguments are empty string or will be empty string
+    if not location or not location.strip():
+        return None
+
+    # create
+    city = ""
+    state = ""
+
+    location = location.lower().strip()
+    parts = location.split(",")
+
+    if len(parts) == 1:
+        city = parts[0].strip()
+    elif len(parts) == 2:
+        city = parts[0].strip()
+        state = parts[1].strip()
+    else:
+        return None
+
+    # print(f"city: {city}, state: {state}}")
+
+    # store stripped result in Location tuple
+    return Location(city, state)
+
+
+def access_weather_web(loc: Location):
+    # get base URL
+    base_URL = f"https://www.wunderground.com/weather/{loc.state}/{loc.city}"
     page = requests.get(base_URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
@@ -21,13 +69,19 @@ def call_weather(location):
     # get the parent div that contains the information needed.
     current_condition = soup.find("div", class_="city-conditions")
 
-    city = soup.find_all("div", class_="city-header")
     temp = current_condition.find("span", class_="wu-value")
 
-    # print(city[0].text.strip())
-    # print(temp.text.strip())
+    div_parent = current_condition.find_all('div', class_='conditions-extra')
+    for div in div_parent:
+        p_element = div.find("p")
+        if p_element:
+            condition = p_element.get_text()
 
-    print(f"The weather in {location}, NATION is {temp.text.strip()} F and Clouds:CLOUDS.")
+    print(temp.text.strip())
+    print(condition)
+
+    print(f"The weather in {loc.city}, {loc.state} is {temp.text.strip()} F and Clouds:{condition}.")
+
 
 
 if __name__ == '__main__':
