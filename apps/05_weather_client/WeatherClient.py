42e5761cafece71829688cb0cc8c312b57ea5cbd
Irsal Lazuard Izzatillah <91246036+IrsalLaz@users.noreply.github.com>
2023-10-21 19:27:53 +0700

    WeatherClient - fix comment

--- apps/05_weather_client/WeatherClient.py
+++ apps/05_weather_client/WeatherClient.py
@@ -34,7 +34,7 @@ def weather_api(city_name):
 
         city = data["name"]
         country = data["sys"]["country"]
-        temp = data["main"]["temp"]     # convert to celsius or fahrenheit
+        temp = data["main"]["temp"]     # convert to fahrenheit
         temp = temp_converter(temp)
         cloud = data["weather"][0]["main"]
 
